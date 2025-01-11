2a1ef2c196bdeec9527d1363f8e935cbffd36f24
Benjamin <casco033@gmail.com>
2025-01-11 16:17:02 -0700

    completed assignment with comments

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,40 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // initialize random number generator 
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101); 
+        int userGuess = 0;
+
+        Console.WriteLine("Try to guess my number from 1-100");
+        // game loop
+        while (userGuess != randomNumber)
+        {
+            Console.Write("Enter your guess: ");
+            string guess = Console.ReadLine();
+
+            if (int.TryParse(guess, out userGuess))
+            {
+                if (userGuess < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userGuess > randomNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                // once number is guessed this will be displayed on the console
+                {
+                    Console.WriteLine($"Congratulations! You guessed my number {randomNumber}!");
+                }
+            }
+            else
+            // if nan is given by user
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
     }
 }
